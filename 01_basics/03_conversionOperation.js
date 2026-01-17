0bbe888cbd5f48806d94f16f98444078dee458f9
mushkaann <muskan9753864@gmail.com>
2026-01-17 19:50:07 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "33abcd"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+//"33"=>33
+//"33abcd"=>NaN
+//true=>1; false=>0
+
+let isLoggedIn = "Muskan"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=>true; 0=>false
+//""=>false
+//"Muskan"=>true

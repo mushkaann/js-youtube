3b49a15a97d503a5263faf1fe59998b7224de8b5
mushkaann <muskan9753864@gmail.com>
2026-01-17 19:13:48 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 14467
+let accountEmail = "muskan03.com"
+var accountPassword = "345656"
+accountCity = "jamui"
+let accountState;
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
